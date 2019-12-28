a51031dc7079edfd5ff6425203815f7ab88d9869
Paolo Combi <paolo@combi.li>
2019-12-28 14:53:52 +0100

    update iterator class

--- src/Component/PandoIterator.php
+++ src/Component/PandoIterator.php
@@ -21,39 +21,59 @@ class PandoIterator implements PandoIteratorInterface
     /**
      * @var int
      */
-    private $position;
+    private $position  = 0;
 
     /**
      * @var bool
      */
     private $reverse;
 
+    /**
+     * PandoIterator constructor.
+     * @param PandoInterface $pando
+     * @param bool $reverse
+     */
     public function __construct(PandoInterface $pando, bool $reverse = false)
     {
         $this->pando = $pando;
         $this->reverse = $reverse;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function current(): ?PandoInterface
     {
         return $this->pando->children() ? $this->pando->children()[$this->position] : null;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function next(): void
     {
         $this->position = $this->position + ($this->reverse ? -1 : 1);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function key(): int
     {
         return is_null($this->position) ? 0 : $this->position;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function valid(): bool
     {
         return $this->pando->children() && isset($this->pando->children()[$this->position]);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function rewind(): void
     {
         $this->position = $this->reverse ? $this->pando->count() - 1 : 0;
