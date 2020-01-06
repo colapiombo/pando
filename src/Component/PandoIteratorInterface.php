4d54f695718d55f5f248d8071b25757e644a4e2e
Paolo Combi <paolo@combi.li>
2020-01-06 19:48:45 +0200

    update Iterator class with PHP CS FIXER

--- src/Component/PandoIteratorInterface.php
+++ src/Component/PandoIteratorInterface.php
@@ -1,46 +1,52 @@
 <?php
+
 declare(strict_types=1);
+
 /**
  *
+ * Pando 2020 — NOTICE OF MIT LICENSE
+ * @copyright 2019-2020 (c) Paolo Combi (https://combi.li)
  * @link    https://github.com/MarshallJamesRaynor/pando
  * @author  Paolo Combi <paolo@combi.li>
  * @license https://github.com/MarshallJamesRaynor/pando/blob/master/LICENSE (MIT License)
- * @package Component
+ *
  */
 
 namespace Pando\Component;
 
 interface PandoIteratorInterface extends \Iterator
 {
-
     /**
-     * Return the current element
-     * @return PandoInterface|null
+     * Return the current element.
      */
     public function current(): ?PandoInterface;
 
     /**
-     * Move forward to next element
-     * @return void Any returned value is ignored.
+     * Move forward to next element.
+     *
+     * @return void any returned value is ignored
      */
     public function next(): void;
 
     /**
-     * Return the key of the current element
-     * @return int scalar on success, or null on failure.
+     * Return the key of the current element.
+     *
+     * @return int scalar on success, or null on failure
      */
     public function key(): int;
 
     /**
-     * Checks if current position is valid
+     * Checks if current position is valid.
+     *
      * @return bool The return value will be casted to boolean and then evaluated.
-     * Returns true on success or false on failure.
+     *              Returns true on success or false on failure.
      */
     public function valid(): bool;
 
     /**
-     * Rewind the Iterator to the first element
-     * @return void Any returned value is ignored.
+     * Rewind the Iterator to the first element.
+     *
+     * @return void any returned value is ignored
      */
     public function rewind(): void;
 }
