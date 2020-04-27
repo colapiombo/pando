03a2684fbd62638aa796b54649fb1fdc38cd2d75
Paolo Combi <paolo@combi.li>
2020-04-27 09:26:40 +0300

    create builder interface

---
+++ src/Api/Service/Builder/BuilderInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ *
+ * Pando NOTICE OF MIT LICENSE
+ *
+ * @copyright Paolo Combi (https://combi.li)
+ * @link    https://github.com/colapiombo/pando
+ * @author  Paolo Combi <paolo@combi.li>
+ * @license https://github.com/colapiombo/pando/blob/master/LICENSE (MIT License)
+ *
+ *
+ */
+
+namespace Pando\Api\Service\Builder;
+
+interface BuilderInterface
+{
+    public static function create();
+}
